935e79c63d18958cf0eca6d55107dc5f702572c6
faiz <razafaiz815@gmail.com>
2021-01-19 21:54:16 +0500

    Formik with Yup added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import FormikForm from './Forms/FormikForm';
+import FormikWithYup from './Forms/FormikWithYup';
 
 const App = () => {
     return (
         <div>
-            <h1>Formik Form</h1>
-            <FormikForm/>       
+            {/* <h1>Formik Form</h1>
+            <FormikForm/>  */}
+            <h1>Formik Form with Yup</h1>
+            <FormikWithYup/>      
         </div>
     )
 }
---
+++ src/Forms/FormikWithYup.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function FormikWithYup() {
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required !"),
+        lastName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required !!"),
+        email: Yup.string()
+          .email("Invalid email address")
+          .required("Required !"),
+        password: Yup.string()
+          .min(4, "Too short")
+          .max(15, "Too long")
+          .required("Required !"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          console.log("VALUES >>> ", values);
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" />
+        <ErrorMessage name="firstName" />
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" />
+        <ErrorMessage name="lastName" />
+        <label htmlFor="email">Email</label>
+        <Field name="email" type="text" />
+        <ErrorMessage name="email" />
+        <label htmlFor="password">Password</label>
+        <Field name="password" type="text" />
+        <ErrorMessage name="password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default FormikWithYup;
